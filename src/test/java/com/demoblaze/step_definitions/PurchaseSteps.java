73b5ea57b5877a3ee96d55c433c24a7fe869123b
fdonkar <fdonkar@gmail.com>
2022-02-06 18:22:44 +0300

    configuration.properties and utilities added

---
+++ src/test/java/com/demoblaze/step_definitions/PurchaseSteps.java
@@ -0,0 +1,17 @@
+package com.demoblaze.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class PurchaseSteps {
+
+    @Given("User is on the homepage")
+    public void user_is_on_the_homepage() {
+
+    }
+    @When("User adds some {string} from {string}")
+    public void user_adds_some_from(String product, String category) {
+
+    }
+
+}
--- src/test/resources/features/purchase.feature
+++ src/test/resources/features/purchase.feature
@@ -1,8 +1,9 @@
 Feature: Adidas Order Placement Functionalities
   Agile Story: As a user, I should be able to place order from website so that
               I can shop online
+  @wip
   Scenario: Placing a successful order
     Given User is on the homepage
-    When User adds some product from "category"
+    When User adds some "product" from "category"
 
 
