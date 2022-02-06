6855cb9b1a61149ae148c619f6e99c078540c2f9
fdonkar <fdonkar@gmail.com>
2022-02-06 22:29:38 +0300

    json and html reports added

--- src/test/java/com/demoblaze/runners/CukesRunner.java
+++ src/test/java/com/demoblaze/runners/CukesRunner.java
@@ -6,6 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {"html:target/cucumber-report.html",
+                  "json:target/cucumber.json"},
         features = "src/test/resources/features",
         glue = "com/demoblaze/step_definitions",
         dryRun = false,
